eac712b83f53c15997c18675cfe1b90dfd2bfa8c
Doyun Yu <doyun0109@icloud.com>
2023-12-01 09:36:54 +0900

    create color palette

---
+++ src/themes/colors.ts
@@ -0,0 +1,31 @@
+const colors = {
+  // red50 ~ red900
+  red50: "#F8E9E9",
+  red100: "#E8BBBC",
+  red200: "#DD9A9B",
+  red300: "#CD6C6E",
+  red400: "#C45051",
+  red500: "#B52426",
+  red600: "#A52123",
+  red700: "#811A1B",
+  red800: "#641415",
+  red900: "#4C0F10",
+
+  // gray50 ~ gray900
+  gray50: "#F8FAFB",
+  gray100: "#F3F5F6",
+  gray200: "#EAEFF2",
+  gray300: "#DCE3E7",
+  gray400: "#C6CED1",
+  gray500: "#B1B6B7",
+  gray600: "#909696",
+  gray700: "#7E8483",
+  gray800: "#353839",
+  gray900: "#1D1F1F",
+
+  //white, black
+  white: "#FFFFFF",
+  black: "#000000",
+}
+
+export default colors;
