ed49de8b5a3a7ba247cf4720cc7da3c148e7ee53
Doyun Yu <doyun0109@icloud.com>
2023-12-01 09:37:55 +0900

    create atomic components

---
+++ src/components/base/Atomic/Container.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+interface ContainerProps {
+  gap?: number;
+  justifyContent?:
+    | 'center'
+    | 'flex-start'
+    | 'flex-end'
+    | 'space-between'
+    | 'space-around'
+    | 'space-evenly';
+  fill?: boolean;
+  alignItems?: 'center' | 'flex-start' | 'flex-end' | 'stretch' | 'baseline';
+  padding?: [number, number] | [number];
+  wrap?: boolean;
+}
+
+const base = styled.div<ContainerProps>`
+  display: flex;
+  gap: ${props => props.gap || 0}px;
+  justify-content: ${props => props.justifyContent || 'flex-start'};
+  align-items: ${props => props.alignItems || 'flex-start'};
+  padding: ${props =>
+          `${props.padding ? props.padding[0] : 0}px ${props.padding ? props.padding[1] || props.padding[0] : 0}px`
+  };
+  flex-wrap: ${props => props.wrap ? 'wrap' : 'nowrap'};
+`;
+
+export const Column = styled(base)`
+  flex-direction: column;
+`;
+export const Row = styled(base)`
+  flex-direction: row;
+`;
---
+++ src/components/base/Atomic/Text.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import colors from '@/themes/colors';
+
+interface TextProps {
+  size?: number;
+  weight?: number;
+  color?: string;
+}
+
+export const Text = styled.p<TextProps>`
+  font-weight: ${props => props.weight || 500};
+  font-size: ${props => props.size || 14}px;
+  color: ${props => props.color || colors.gray900};
+`;
