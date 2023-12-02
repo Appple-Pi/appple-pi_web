ffd3f44a33c06e3ef88f719098512e9c9b11f479
Doyun Yu <doyun0109@icloud.com>
2023-12-02 18:13:03 +0900

    edit text atomic

--- src/components/base/Atomic/Text.tsx
+++ src/components/base/Atomic/Text.tsx
@@ -11,4 +11,5 @@ export const Text = styled.p<TextProps>`
   font-weight: ${props => props.weight || 500};
   font-size: ${props => props.size || 14}px;
   color: ${props => props.color || colors.gray900};
+  white-space: pre-line;
 `;
