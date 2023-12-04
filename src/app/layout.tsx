cad3e9d70d5b2a33c5b9e484faa7734f4ea6671f
Doyun Yu <doyun0109@icloud.com>
2023-12-04 10:49:12 +0900

    change language

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="kr">
     <body className={suit.className}>
     <StyledComponentsRegistry>
       <NavBar/>
